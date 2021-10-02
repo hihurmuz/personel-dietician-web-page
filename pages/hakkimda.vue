67292b91017e9a50882d4efdbd83293fe9b348d0
halilhurmuz <halilhurmuz@yandex.com>
2021-10-03 00:05:29 +0300

    fix: my services page images add

--- pages/hakkimda.vue
+++ pages/hakkimda.vue
@@ -2,15 +2,14 @@
   <v-container>
     <v-row no-gutters>
       <v-col class="d-flex flex-column align-center my-4 mx-4">
-        <v-img
-          height="300"
-          width="400"
-          :src="myImage"
-        />
+        <v-img height="300" width="400" :src="myImage" />
       </v-col>
       <v-col>
         <p>
-          İlk ve orta öğrenimini İstanbul’da tamamladıktan sonra 2013 yılında İstanbul Çapa Fen Lisesi’nden mezun oldum. 2017 yılında Marmara Üniversitesi Beslenme ve Diyetetik bölümünü diyetisyen ünvanı alarak başarıyla bitirdim.
+          İlk ve orta öğrenimini İstanbul’da tamamladıktan sonra 2013 yılında
+          İstanbul Çapa Fen Lisesi’nden mezun oldum. 2017 yılında Marmara
+          Üniversitesi Beslenme ve Diyetetik bölümünü diyetisyen ünvanı alarak
+          başarıyla bitirdim.
         </p>
         <ul>
           <li>İstanbul Üniversitesi Çapa Tıp Fakültesi</li>
@@ -19,13 +18,24 @@
           <li>Özel Pendik Bölge Hastanesi</li>
         </ul>
         <p>
-          başta olmak üzere pek çok farklı klinik ve özel merkezde yaptığı stajlar ile teorik ve pratik bilgiyi birleştirdim, birçok farklı danışan görme fırsatı yakaladım.
+          başta olmak üzere pek çok farklı klinik ve özel merkezde yaptığı
+          stajlar ile teorik ve pratik bilgiyi birleştirdim, birçok farklı
+          danışan görme fırsatı yakaladım.
         </p>
         <p>
-          Lisans eğitimimi <strong>Üniversite Öğrencileri’nin Kahvaltı Yapma Durumunun Beden Kitle İndeksi ve Okul Başarısına Etkisi</strong> adlı tez çalışmamla bitirdikten sonra Kadın ve Yaşam Spor Merkezi’nde mesleğine ilk adımımı attım. Ardından Özel Dr. Fevzi Özgönül Kliniğinde 1 yıl süreyle çalışmıştım.
+          Lisans eğitimimi
+          <strong
+            >Üniversite Öğrencileri’nin Kahvaltı Yapma Durumunun Beden Kitle
+            İndeksi ve Okul Başarısına Etkisi</strong
+          >
+          adlı tez çalışmamla bitirdikten sonra Kadın ve Yaşam Spor Merkezi’nde
+          mesleğine ilk adımımı attım. Ardından Özel Dr. Fevzi Özgönül
+          Kliniğinde 1 yıl süreyle çalışmıştım.
         </p>
         <p>
-          Hala poliklinik hizmetlerim sürdürmekteyim. Aynı zamanda <strong>2017 yılından beri</strong> sürdürdüğüm online diyet hizmetine devam etmekteyim.
+          Hala poliklinik hizmetlerim sürdürmekteyim. Aynı zamanda
+          <strong>2017 yılından beri</strong> sürdürdüğüm online diyet hizmetine
+          devam etmekteyim.
         </p>
       </v-col>
     </v-row>
@@ -34,11 +44,11 @@
 
 <script>
 export default {
-name: "Hakkimda",
-  data () {
+  name: 'Hakkimda',
+  data() {
     return {
-      myImage: require('@/static/images/betull.png')
+      myImage: require('@/static/images/betull.png'),
     }
-  }
+  },
 }
 </script>
--- pages/hizmetlerim.vue
+++ pages/hizmetlerim.vue
@@ -1,45 +1,72 @@
 <template>
   <v-container>
-    <h2>
-      Hizmetlerim
-    </h2>
-    <ul>
-      <li>
-        Obezite  ve Kilo Kontrolü
-      </li>
-      <li>
-        Kilo alma ve Kilo Verme Programları
-      </li>
-      <li>
-        Hastalıklarda Beslenme  Tedavisi
-      </li>
-      <li>
-        Diyabetik Bireylerde Beslenme
-      </li>
-      <li>
-        Gebelik Döneminde Beslenme ve Kilo Kontrolü
-      </li>
-      <li>
-        Emziklilik Döneminde Beslenme ve Kilo Kontrolü
-      </li>
-      <li>
-        Yeme Davranış  Bozukluklarında  Beslenme
-      </li>
-      <li>
-        Çocukluk çağı ve Ergenlik Döneminde Beslenme
-      </li>
-      <li>
-        Kurumsal Beslenme Danışmanlığı
-      </li>
-      <li>
-        Online Diyet ve Beslenme Danışmanlığı
-      </li>
-    </ul>
+    <h2>Hizmetlerim</h2>
+    <v-row>
+      <v-card
+        v-for="item in myServices"
+        :key="item.title"
+        class="mx-auto my-12"
+        max-width="500"
+      >
+        <v-img height="400" :src="require(`@/static/images/${item.imgPath}`)" />
+
+        <v-card-text>
+          <h3>{{ item.title }}</h3>
+        </v-card-text>
+      </v-card>
+    </v-row>
   </v-container>
 </template>
 
 <script>
 export default {
-name: "Hizmetlerim"
+  // require(`@/static/robil/robil_hello.png`)
+  name: 'Hizmetlerim',
+  data() {
+    return {
+      myServices: [
+        {
+          title: 'Obezite ve Kilo Kontrolü',
+          imgPath: 'obezite.jpg',
+        },
+        {
+          title: 'Kilo alma ve Kilo Verme Programları',
+          imgPath: 'kilo-kontrol.jpg',
+        },
+        {
+          title: 'Hastalıklarda Beslenme Tedavisi',
+          imgPath: 'hastaliklarda-beslenme.jpg',
+        },
+        {
+          title: 'Diyabetik Bireylerde Beslenme',
+          imgPath: 'diyabet-beslenme.jpg',
+        },
+        {
+          title: 'Gebelik Döneminde Beslenme ve Kilo Kontrolü',
+          imgPath: 'hamile.jpg',
+        },
+        {
+          title: 'Emziklilik Döneminde Beslenme ve Kilo Kontrolü',
+          imgPath: 'emziklik.jpg',
+        },
+        {
+          title: 'Yeme Davranış Bozukluklarında Beslenme',
+          imgPath: 'yeme-bozukugu.jpg',
+        },
+        {
+          title: 'Çocukluk çağı ve Ergenlik Döneminde Beslenme',
+          imgPath: 'ergen.jpg',
+        },
+        {
+          title: 'Kurumsal Beslenme Danışmanlığı',
+          imgPath: 'kurumsal.jpg',
+        },
+        {
+          title: 'Online Diyet ve Beslenme Danışmanlığı',
+          imgPath: 'online-diyet.png',
+        },
+      ],
+    }
+  },
 }
 </script>
